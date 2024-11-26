3e303458e0e4318df423b0ba1c71c128722fb719
ZephyrCodesStuff <zeph@zephs.me>
2024-11-26 01:28:40 +0100

    fix: remove commented-out code

--- src/main.rs
+++ src/main.rs
@@ -16,26 +16,6 @@ use actix_web::{middleware::Logger, web::Data, App, HttpServer};
 use database::Database;
 
 
-// #[post("/{clan_manager_update_view}/{sec_func}/{func}")]
-// async fn sec_func(body: Bytes, func: Path<((), (), String)>) -> impl Responder {
-//     let body = String::from_utf8_lossy(&body);
-//     log::debug!("body: {}", body);
-
-//     let response = fs::read_to_string(format!("responses/{}.xml", func.2))
-//         .unwrap_or_else(|_| panic!("Could not read file responses/{}.xml", func.2));
-
-//     let date = chrono::Utc::now().format("%a, %d %b %Y %H:%M:%S GMT").to_string();
-//     HttpResponse::Ok()
-//         .append_header(("Message-Type", "x-ps3-clan"))
-//         .append_header(("Version", "1.00"))
-//         .append_header(("Ignore-Level", "normal"))
-//         .append_header(("Content-Type", "application/x-ps3-clan"))
-//         .append_header(("Date", date))
-//         .append_header(("Server", "Apache"))
-//         .keep_alive()
-//         .body(response)
-// }
-
 #[actix_web::main]
 async fn main() -> std::io::Result<()> {
     dotenv::dotenv().ok();
